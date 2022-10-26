99a7e189b1c10f58a5602e22071f3884a4132a88
Sammydepoj <samueladepoju86@yahoo.com>
2022-10-27 00:01:46 +0100

    initial upload

---
+++ classes/Dbh.php
@@ -0,0 +1,20 @@
+<?php
+
+class dbh {
+
+    public $hostname = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $dbname = "zuriphp";
+
+     function set_host_name($givenname){
+        $this->name = $givenname;
+    }
+    function set_user_name($givenname){
+        $this->name = $givenname;
+    }
+
+
+}
+
+
